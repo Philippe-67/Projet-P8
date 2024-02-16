c8670b8e655a5285ca25a688a2c5150af7d48baf
Philippe CHARLES <A114SR@login.axa>
2024-02-16 09:38:40 +0100

    affichage de la dropdownlist avec selection + affichage des rendez-vous du praticien sélectionné.

--- UI/Controllers/PraticienController.cs
+++ UI/Controllers/PraticienController.cs
@@ -67,6 +67,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
+using UI.ViewModels;
 
 public class PraticienController : Controller
 {
@@ -82,53 +83,89 @@ public class PraticienController : Controller
     {
         try
         {
-            //          Si aucun praticien n'est sélectionné, afficher simplement la vue sans données
-            if (!praticienId.HasValue)
+            var viewModel = new PraticienViewModel
             {
-                ViewBag.PraticienList = await GetPraticienListAsync();
-                return View();
+                PraticienList = await GetPraticienListAsync(),
+                SelectedPraticien = null, // Initialiser à null ou récupérer le praticien sélectionné
+                SelectedRdvList = null // Initialiser à null ou récupérer les rendez-vous
+            };
+
+            // Logique pour récupérer les données du praticien et des rendez-vous
+            if (praticienId.HasValue)
+            {
+                viewModel.SelectedPraticien = viewModel.PraticienList.FirstOrDefault(p => p.Id == praticienId);
+                viewModel.SelectedRdvList = await GetRdvListAsync(praticienId);
             }
 
-            //         Si un praticien est sélectionné, récupérer les données filtrées
-            var filteredPraticienList = await GetPraticienListAsync();
-
-            //         Filtrer les praticiens
-            filteredPraticienList = praticienId.HasValue
-                ? filteredPraticienList.Where(p => p.Id == praticienId).ToList()
-                : filteredPraticienList;
-
-            ViewBag.PraticienList = filteredPraticienList; // dropdownlis
-            //       Récupérer les rendez - vous du praticien sélectionné
-            var rendezVousResponse = await _httpClient.GetAsync($"/api/rdv/Praticien/{praticienId}");
-
-            if (rendezVousResponse.IsSuccessStatusCode)
-            {
-                var rendezVousList = await rendezVousResponse.Content.ReadFromJsonAsync<List<Rdv>>();
-                ViewBag.SelectedRdvList = rendezVousList; // stocker les rendez-vous dans ViewBag
-            return View(rendezVousList);
-
-            }
-            else
-            {
-                var errorMessage = await rendezVousResponse.Content.ReadAsStringAsync();
-                throw new Exception(errorMessage);
-            }
-
-            return View(filteredPraticienList);
+            return View(viewModel);
         }
         catch (Exception ex)
         {
             return View("Error", ex.Message);
         }
-    }
+        //    try
+        //    {
+        //        //          Si aucun praticien n'est sélectionné, afficher simplement la vue sans données
+        //        if (!praticienId.HasValue)
+        //        {
+        //            ViewBag.PraticienList = await GetPraticienListAsync();
+        //            return View();
+        //        }
 
-    private async Task<List<Praticien>> GetPraticienListAsync()
+        //        //         Si un praticien est sélectionné, récupérer les données filtrées
+        //        var filteredPraticienList = await GetPraticienListAsync();
+
+        //        //         Filtrer les praticiens
+        //        filteredPraticienList = praticienId.HasValue
+        //            ? filteredPraticienList.Where(p => p.Id == praticienId).ToList()
+        //            : filteredPraticienList;
+
+        //        ViewBag.PraticienList = filteredPraticienList; // dropdownlis
+        //        //       Récupérer les rendez - vous du praticien sélectionné
+        //        var rendezVousResponse = await _httpClient.GetAsync($"/api/rdv/Praticien/{praticienId}");
+
+        //        if (rendezVousResponse.IsSuccessStatusCode)
+        //        {
+        //            var rendezVousList = await rendezVousResponse.Content.ReadFromJsonAsync<List<Rdv>>();
+        //            ViewBag.SelectedRdvList = rendezVousList; // stocker les rendez-vous dans ViewBag
+        //        return View(rendezVousList);
+
+        //        }
+        //        else
+        //        {
+        //            var errorMessage = await rendezVousResponse.Content.ReadAsStringAsync();
+        //            throw new Exception(errorMessage);
+        //        }
+
+        //        return View(filteredPraticienList);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return View("Error", ex.Message);
+        //    }
+        }
+
+        private async Task<List<Praticien>> GetPraticienListAsync()
     {
         var response = await _httpClient.GetAsync("/api/praticien");
 
         if (response.IsSuccessStatusCode)
         {
             return await response.Content.ReadFromJsonAsync<List<Praticien>>();
+        }
+        else
+        {
+            var errorMessage = await response.Content.ReadAsStringAsync();
+            throw new Exception(errorMessage);
+        }
+    }
+    private async Task<List<Rdv>> GetRdvListAsync(int? praticienId)
+    {
+        var response = await _httpClient.GetAsync($"/api/rdv/Praticien/{praticienId}");
+
+        if (response.IsSuccessStatusCode)
+        {
+            return await response.Content.ReadFromJsonAsync<List<Rdv>>();
         }
         else
         {
